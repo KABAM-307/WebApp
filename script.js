e7c1ea83abe4b1f6baf63c0a9d188ad66f137672
AtbTheGreat <annatiffanybenjamin@gmail.com>
2015-02-26 17:49:58 -0500

    Updated the Location/Historical Buttons

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 var main = function() {
+	
+	
+	// these bools determine whether or not the Location/Historical buttons have
+	// been pressed or not
+	var loc = false, hist = false;
+	
 	  $(".button").hover(
 			  function(){
 				  $(this).addClass("active");
@@ -7,6 +13,42 @@ var main = function() {
 				  $(this).removeClass("active");
 			  }
 	  );
+	  
+	  /*
+	   * 
+	   * Clicking the "Location" button
+	   * */
+	  $("#sidebarbutton:first").click(
+			  function(){
+				  hist = false;
+				if(!loc){
+					$("#content>p").text("Search by Location");
+				}
+				else{
+					
+					$("#content>p").text("View data here."); 
+				}
+				loc = !loc;
+			  }
+	  	);
+	  
+	  /*
+	   * Clicking the "Historical" button
+	   * */
+	  $("#sidebarbutton:nth-child(2)").click(
+			  function(){
+				  loc = false;
+				if(!hist){
+					$("#content>p").text("View Historical");  
+				}
+				else{
+					
+					$("#content>p").text("View data here.");
+				} 
+				hist = !hist;
+			  }
+	  	);
+	  
 	  
 };
 $(document).ready(main);
