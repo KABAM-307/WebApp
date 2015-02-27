4d6b1a6c315f449e8acd14ea870fbbdf5d65aa08
AtbTheGreat <annatiffanybenjamin@gmail.com>
2015-02-26 19:04:11 -0500

    Updated Side Buttons

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 var main = function() {
 	
 	
-	// these bools determine whether or not the Location/Historical buttons have
+	// thelse bools determine whether or not the side buttons have
 	// been pressed or not
-	var loc = false, hist = false;
-	
+	var button1Pressed = false, button2Pressed = false, button3Pressed = false;
 	  $(".button").hover(
 			  function(){
 				  $(this).addClass("active");
@@ -16,39 +15,58 @@ var main = function() {
 	  
 	  /*
 	   * 
-	   * Clicking the "Location" button
+	   * Clicking a side button
 	   * */
 	  $("#sidebarbutton:first").click(
 			  function(){
-				  hist = false;
-				if(!loc){
-					$("#content>p").text("Search by Location");
+				  
+				if(!button1Pressed){
+					$("#content>p").text($("#sidebarbutton:first").text());
+					button1Pressed = true;
+				}
+				else{
+					
+					$("#content>p").text("View data here.");  
+					button3Pressed = false; 
+					button2Pressed = false; 
+					button1Pressed = false;
+				}
+			  }
+	  	);
+	  
+	  $("#sidebarbutton:nth-child(2)").click(
+			  function(){
+				  
+				if(!button2Pressed){
+					$("#content>p").text($("#sidebarbutton:nth-child(2)").text()); 
+					button2Pressed = true;
+				}
+				else{
+					
+					$("#content>p").text("View data here.");  
+					button3Pressed = false; 
+					button2Pressed = false; 
+					button1Pressed = false;
+				}
+			  }
+	  	);
+	  
+	  $("#sidebarbutton:nth-child(3)").click(
+			  function(){
+				  
+				if(!button3Pressed){
+					$("#content>p").text($("#sidebarbutton:nth-child(3)").text());
+					button3Pressed = true;
 				}
 				else{
 					
 					$("#content>p").text("View data here."); 
+					button3Pressed = false; 
+					button2Pressed = false; 
+					button1Pressed = false;
 				}
-				loc = !loc;
 			  }
 	  	);
-	  
-	  /*
-	   * Clicking the "Historical" button
-	   * */
-	  $("#sidebarbutton:nth-child(2)").click(
-			  function(){
-				  loc = false;
-				if(!hist){
-					$("#content>p").text("View Historical");  
-				}
-				else{
-					
-					$("#content>p").text("View data here.");
-				} 
-				hist = !hist;
-			  }
-	  	);
-	  
 	  
 };
 $(document).ready(main);
