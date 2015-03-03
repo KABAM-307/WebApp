eb50a4b50031834c88a8dd68f82abb72610e7aaf
aklasa <aklasa@purdue.edu>
2015-03-03 15:43:02 -0500

    Sub nav menu on setup page changes divs

--- script.js
+++ script.js
@@ -71,14 +71,41 @@ var main = function() {
 };
 $(document).ready(main);
 
-function showHistorical() {
+/**
+ * Functions below show divs based on sub navigations buttons
+ */
+
+ // Show historical data filter div on viewdata page
+function view_showHistorical() {
 	document.getElementById("historical").style.display = "block";
 	document.getElementById("location").style.display = "none";
 	document.getElementById("basic").style.display = "none";
 }
-function showLocation() {
+// Show location search filter div on viewdata page
+function view_showLocation() {
 	document.getElementById("location").style.display = "block";
 	document.getElementById("historical").style.display = "none";
 	document.getElementById("basic").style.display = "none";
+}
+// Show create a station div on setup page
+function setup_showCreate() {
+	document.getElementById("basic").style.display = "none";
+	document.getElementById("create").style.display = "block";
+	document.getElementById("source").style.display = "none";
+	document.getElementById("info").style.display = "none";
+}
+// Show view source code div on setup page
+function setup_showSource() {
+	document.getElementById("basic").style.display = "none";
+	document.getElementById("create").style.display = "none";
+	document.getElementById("source").style.display = "block";
+	document.getElementById("info").style.display = "none";
+}
+// Show product info div on setup page
+function setup_showInfo() {
+	document.getElementById("basic").style.display = "none";
+	document.getElementById("create").style.display = "none";
+	document.getElementById("source").style.display = "none";
+	document.getElementById("info").style.display = "block";
 }
 
