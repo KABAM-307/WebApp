2f6edf59888583a9055ddd342f7fc2fc291ab13e
aklasa <aklasa@purdue.edu>
2015-04-05 18:13:30 -0400

    viewdata: added filter fields

--- script.js
+++ script.js
@@ -77,18 +77,6 @@ $(document).ready(main);
  * Functions below show divs based on sub navigations buttons
  */
 
- // Show historical data filter div on viewdata page
-function view_showHistorical() {
-	document.getElementById("historical").style.display = "block";
-	document.getElementById("location").style.display = "none";
-	document.getElementById("basic").style.display = "none";
-}
-// Show location search filter div on viewdata page
-function view_showLocation() {
-	document.getElementById("location").style.display = "block";
-	document.getElementById("historical").style.display = "none";
-	document.getElementById("basic").style.display = "none";
-}
 // Show create a station div on setup page
 function setup_showCreate() {
 	document.getElementById("basic").style.display = "none";
