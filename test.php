bac7ad0fd132c7754840af68de4c51d90d66a016
aklasa <aklasa@purdue.edu>
2015-04-06 18:57:31 -0400

    Testing include

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php include_once('./header.php'); ?>
+
+<div id="content" class="content">
+	<h1>
+		Welcome! Personal Pi in the Sky
+	</h1>
+	<a href="test.php">Test include page</a>
+
+	<script type="text/javascript">
+		getCurrent();
+	</script>
+	<p><span id="currentData"></span></p>
+
+	<p>
+		<br>
+		Personal Pi in the Sky was created by Purdue University Computer Science students
+		as part of a semester long software engineering class. To learn more about the class and our team, see the About Us page on our website.<br><br>
+	</p>
+</div>
+
+<?php include_once('./footer.php'); ?>
