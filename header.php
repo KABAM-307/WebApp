820c32fb7330c611731ce5d63a68ff02d0c3ff77
aklasa <aklasa@purdue.edu>
2015-04-06 22:08:58 -0400

    Testing include

--- header.php
+++ header.php
@@ -88,31 +88,3 @@
 			</span>
 		</a>
 </div>
-
-
-<div id="content" class="content">
-	<h1>
-		Welcome! Personal Pi in the Sky
-	</h1>
-
-	<script type="text/javascript">
-		getCurrent();
-	</script>
-	<p><span id="currentData"></span></p>
-
-	<p>
-		<br>
-		Personal Pi in the Sky was created by Purdue University Computer Science students
-		as part of a semester long software engineering class. To learn more about the class and our team, see the About Us page on our website.<br><br>
-	</p>
-</div>
-
-<div id="footer">
-</div>
-
-<link rel="stylesheet" type="text/css" href="style.css"/>
- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script type="text/javascript" src="script.js"></script>
-</body>
-
-</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+
+<div id="content" class="content">
+	<h1>
+		Welcome! Personal Pi in the Sky
+	</h1>
+
+	<script type="text/javascript">
+		getCurrent();
+	</script>
+	<p><span id="currentData"></span></p>
+
+	<p>
+		<br>
+		Personal Pi in the Sky was created by Purdue University Computer Science students
+		as part of a semester long software engineering class. To learn more about the class and our team, see the About Us page on our website.<br><br>
+	</p>
+</div>
