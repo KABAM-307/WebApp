f33ab160254e2c7cbcd8f44d86de58bee7a560e7
bkuehn <bridgettekuehn@gmail.com>
2015-04-07 16:44:10 -0400

    Editing Location Coordinates for current data

--- currentData.php
+++ currentData.php
@@ -6,9 +6,9 @@
  * Time: 3:33 PM
  */
 include 'Model.php';
-
-$lat = $_POST["latitude"];
-$long = $_POST["longitude"];
+echo "Inside currentData.php";
+$lat = $_GET["latitude"];
+$long = $_GET["longitude"];
 #asking for current data from the database
 #send location to our model with a false
 #to show only to get the most current data
