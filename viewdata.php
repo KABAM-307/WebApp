07f137d8a6741c58b5bee997a135ab679923ac6b
aklasa <aklasa@purdue.edu>
2015-04-07 00:25:29 -0400

    work on maxDate in filter

--- viewdata.php
+++ viewdata.php
@@ -64,7 +64,7 @@
 				print_r($date);
 				echo '<br>';
 				$maxDate = $date[year]. "-" . $date[mon] . "-" . $date[day];
-				echo '$maxDate';
+				echo "%$maxDate";
 				echo 'low: <input type="date" name="lowDate" size="7" min="2015-1-1" max=$maxDate value="2015-1-1">';
 				echo 'high: <input type="date" name="highDate" size="7" min="2015-1-1" max=$maxDate value=$maxDate>';
 			?>
