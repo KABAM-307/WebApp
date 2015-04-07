168283b64d83bc6e23d3147fe5d9f6f96c780556
aklasa <aklasa@purdue.edu>
2015-04-07 19:15:20 -0400

    remove echo statements

--- currentData.php
+++ currentData.php
@@ -6,14 +6,14 @@
  * Time: 3:33 PM
  */
 include 'Model.php';
-echo "Inside currentData.php";
+//echo "Inside currentData.php";
 $lat = $_GET["latitude"];
 $long = $_GET["longitude"];
 #asking for current data from the database
 #send location to our model with a false
 #to show only to get the most current data
-echo "Found latitude: " . $lat . "\n";
-echo "Found longitude: " . $long . "\n";
+//echo "Found latitude: " . $lat . "\n";
+//echo "Found longitude: " . $long . "\n";
 $data_to_post = pullCurrentData($lat, $long);
 #post the data
 postCurrentData($data_to_post);
