8a52a8d6c6dbefb123583ec3eec937363f1509e5
aklasa <aklasa@purdue.edu>
2015-04-06 23:59:42 -0400

    working on max date in form

--- viewdata.php
+++ viewdata.php
@@ -59,9 +59,11 @@
 			Enter a date range:<br>
 			<?php
 				print_r(getdate());
+				$date = getdate();
+				echo print_r(date);
+				low: <input type="date" name="lowDate" size="7" min="2015-1-1" max="2015-1-1" value="2015-1-1">
+				high: <input type="date" name="highDate" size="7" min="2015-1-1" max="2015-1-1" value="2015-1-1">
 			?>
-			low: <input type="date" name="lowDate" size="7" min="2015-1-1" max="2015-1-1" value="2015-1-1">
-			high: <input type="date" name="highDate" size="7" min="2015-1-1" max="2015-1-1" value="2015-1-1">
 			<br>
 		</fieldset>
 		<br>
