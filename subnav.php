a6d76fd3abbfd639cd10ca3c219f514ff402c24c
aklasa <aklasa@purdue.edu>
2015-04-06 23:09:13 -0400

    added subnav.php

---
+++ subnav.php
@@ -0,0 +1,28 @@
+<div id="sub_nav">
+	<!-- Optional navigation bar here -->
+	<a href="index.php">
+			<span class="button hidden" id="sidebarbutton">
+				Home
+			</span>
+		</a>
+		<a href="viewdata.php">
+			<span class="button hidden" id="sidebarbutton">
+				View Data
+			</span>
+		</a>
+		<a href="setup.php">
+			<span class="button hidden" id="sidebarbutton">
+				Set-up
+			</span>
+		</a>
+		<a href="aboutus.php">
+			<span class="button hidden" id="sidebarbutton">
+				About
+			</span>
+		</a>
+		<a href="contactus.php">
+			<span class="button hidden" id="sidebarbutton">
+				Contact Us
+			</span>
+		</a>
+</div>
