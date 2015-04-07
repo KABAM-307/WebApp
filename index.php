e07ecfa5991fc5b89c56e249ef91539ddc66961e
aklasa <aklasa@purdue.edu>
2015-04-07 16:30:00 -0400

    Get lat and long from web app

--- index.php
+++ index.php
@@ -2,6 +2,27 @@
 
 <?php include_once('./subnav.php'); ?>
 
+<script>
+	function getCurrent() {
+		if (navigator.geolocation) {
+	  		navigator.geolocation.getCurrentPosition(function(position) {
+    			var lat = position.coords.lat;
+       			var lon = position.coords.lon;
+       		});
+   		}
+
+	    var xmlhttp = new XMLHttpRequest();
+	    xmlhttp.onreadystatechange = function() {
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				document.getElementById("currentData").innerHTML = xmlhttp.responseText;
+			}
+   		}
+
+    	xmlhttp.open("GET", "currentData.php?latitude=" + lat + "&longitude=" + lon);
+		xmlhttp.send();
+	}
+</script>
+	
 <div id="content" class="content">
 	<h1>
 		Welcome! Personal Pi in the Sky
