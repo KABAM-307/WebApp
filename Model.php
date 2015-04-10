adea2f78e85c3cea2514f1fffab569915956a77e
bkuehn <bridgettekuehn@gmail.com>
2015-04-10 01:29:48 -0400

    Testing filter by date

--- Model.php
+++ Model.php
@@ -144,6 +144,7 @@ function getZip($city, $state)
     $result1[] = $result['results'][0];
     $result2[] = $result1[0]['geometry'];
     $result3[] = $result2[0]['location'];
+    echo $result3['lat'] . ", " . $result3['lng'] . "\n";
     $url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" . $result3['lat'] . "," . $result3['lng'] . "&sensor=true";
     $result_string = file_get_contents($url);
     $result = json_decode($result_string, true);
