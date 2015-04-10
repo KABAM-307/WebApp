5592dcace19c84d537a29879eb330a16d74494eb
bkuehn <bridgettekuehn@gmail.com>
2015-04-10 01:14:37 -0400

    Testing adding data to server

--- piData.php
+++ piData.php
@@ -6,7 +6,9 @@
  * Time: 3:30 PM
  */
     include 'Model.php';
-    $jsonstr = $_POST["json"];
+    //$jsonstr = $_POST["json"];
+    $jsonstr = "{\n\"RPiData\": {\n\"type\":\"info\",\n\"pi_ID\":\"json1\",\n\"alias\":\"json_tester1\",\n\"owner\":\"bkuehn\",\n\"location\":47906,\n\"share\":0\n}\n}";
+    echo $jsonstr;
     addJSONData($jsonstr);
 ?>
 
