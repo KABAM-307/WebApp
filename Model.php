d3c84a60ab8acde32641e670315d09106c365a38
bkuehn <bridgettekuehn@gmail.com>
2015-04-10 02:14:50 -0400

    Testing filter by location

--- Model.php
+++ Model.php
@@ -149,13 +149,13 @@ function getZip($city, $state)
     $result1[] = $result['results'][0];
     $result2[] = $result1[0]['geometry'];
     $result3[] = $result2[0]['location'];
-    $url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" . $result3[0]['lat'] . "," . $result3[0]['lng'] . "&sensor=true";
+    $url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" . $result3[0]['lat'] . "," . $result3[0]['lng'] . "&sensor=true&components=types:postal_code";
     $result_string = file_get_contents($url);
     echo $result_string . "\n\n\n";
     $result = json_decode($result_string, true);
     $result4[] = $result['results'][0];
     $result5[] = $result4[0]['address_components'][0];
-    $zip = $result5[1]['long_name'];
+    $zip = $result5[0]['long_name'];
     echo "\n" . $zip;
     return $zip;
 }
