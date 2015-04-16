ff36c3bc5bc0e3f214e4913f6a5ad453dc62ab30
bkuehn <bridgettekuehn@gmail.com>
2015-04-16 16:41:56 -0400

    Attempting to fix location parsing

--- Model.php
+++ Model.php
@@ -159,7 +159,7 @@ function getZip($city, $state)
     $index = 0;
     for ($i = 0; $i < 15; $i++) {
         $result5[] = $result4[0]['address_components'][$i];
-        var_dump($result5[0]);
+        var_dump($result4[0]['address_components'][$i]);
         echo "<br><br>";
         if ($result5[0]['types'][0] == "postal_code") {
             echo $i;
