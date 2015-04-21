57c1b172c2bc9680a953dd6478e6afdc0942db07
AtbTheGreat <annatiffanybenjamin@gmail.com>
2015-04-21 16:29:10 -0400

    writing via json
    does this work?

--- index.php
+++ index.php
@@ -25,15 +25,18 @@
 		*/
 		$.getJSON("currentData.php?latitude=" + lat + "&longitude=" + lon, function(data){
 			
-			console.log(data);
+			//console.log(data);
 			
-			
-			
+			$("#data").html("<p>Temperature: "+data.temp+" degrees Fahrenheit</p>");
+			$("#data").append("<p>Humidity: "+data.humidity+"%</p>");
+			$("#data").append("<p>Wind speed: "+data.wind+" mph</p>");
+			$("#data").append("<p>Light: "+data.light+" lux</p>");
+			$("#data").append("<p>ZIP code: "+data.zipcode+"</p>");
 			
 		});
 		
 		
-        xmlhttp.send();
+        //xmlhttp.send();
 
       });
     }
