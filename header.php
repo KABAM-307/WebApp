9747996212df52a98bde13acf2c6683a6fce6c24
Kathryn <kathryn.fr13@gmail.com>
2015-05-01 18:37:58 -0400

    Adding option for favorite WeatherStation

--- header.php
+++ header.php
@@ -32,6 +32,11 @@
         Set-up
       </button>
     </a>
+	<a href="alias.php">
+      <button class="floatLeft" id="navbarbutton">
+        View Specific WS
+      </button>
+    </a>
     <a href="aboutus.php">
       <button class="floatRight" id="navbarbutton">
         About
