da8f7b30bf07330ee7dabfa613efdf83278cfb88
aklasa <aklasa@purdue.edu>
2015-05-01 15:53:05 -0400

    Adding filter check boxes into form

--- viewdata.php
+++ viewdata.php
@@ -5,18 +5,18 @@
 <div id="basic" class="content">
 	<h1>View Data</h1>
 	<p>Search and filer historical weather data.</p>
-	<label>
-		<input type="checkbox" id="aliasCheck">alias</input>
-		<input type="checkbox" id="locCheck">location</input>
-		<input type="checkbox" id="tempCheck">temperature</input>
-		<input type="checkbox" id="humidCheck">humidity</input>
-		<input type="checkbox" id="lightCheck">light</input>
-		<input type="checkbox" id="windCheck">wind</input>
-		<input type="checkbox" id="dateCheck">date</input>
-	</label>
 	<br>
 	
 	<form action="filterData.php" method="post">
+		<label>
+			<input type="checkbox" id="aliasCheckid" name="aliasCheck">alias</input>
+			<input type="checkbox" id="locCheckid" name="locCheck">location</input>
+			<input type="checkbox" id="tempCheckid" name="tempCheck">temperature</input>
+			<input type="checkbox" id="humidCheckid" name="humidCheck">humidity</input>
+			<input type="checkbox" id="lightCheckid" name="lightCheck">light</input>
+			<input type="checkbox" id="windCheckid" name="windCheck">wind</input>
+			<input type="checkbox" id="dateCheckid" name="dateCheck">date</input>
+		</label>
 		<fieldset id="alias" style="display:none">
 			<legend>Weather Station Alias</legend>
 			Enter a weather station alias:<br>
@@ -33,29 +33,29 @@
 		<fieldset id="temp" style="display:none">
 			<legend>Temperature</legend>
 			Enter a temperature range:<br>
-			low: <input type="number" name="lowTemp" size="7" min="-150" max="150" value="0">
-			high: <input type="number" name="highTemp" size="7" min="-150" max="150" value="100">
+			low: <input type="number" name="lowTemp" size="7" min="-40" max="120" value="30">
+			high: <input type="number" name="highTemp" size="7" min="-40" max="120" value="80">
 			<br>
 		</fieldset>
 		<fieldset id="humid" style="display:none">
 			<legend>Humidity</legend>
 			Enter a humidity range:<br>
-			low: <input type="number" name="lowHumid" size="7" min="-150" max="150" value="0">
-			high: <input type="number" name="highHumid" size="7" min="-150" max="150" value="100">
+			low: <input type="number" name="lowHumid" size="7" min="0" max="100" value="20">
+			high: <input type="number" name="highHumid" size="7" min="0" max="100" value="80">
 			<br>
 		</fieldset>
 		<fieldset id="light" style="display:none">
 			<legend>Light</legend>
 			Enter a light range:<br>
-			low: <input type="number" name="lowLight" size="7" min="-150" max="150" value="0">
-			high: <input type="number" name="highLight" size="7" min="-150" max="150" value="100">
+			low: <input type="number" name="lowLight" size="7" min="0" max="100" value="0">
+			high: <input type="number" name="highLight" size="7" min="0" max="100" value="100">
 			<br>
 		</fieldset>
 		<fieldset id="wind" style="display:none">
 			<legend>Wind</legend>
 			Enter a wind speed range:<br>
-			low: <input type="number" name="lowSpeed" size="7" min="0" max="150" value="0">
-			high: <input type="number" name="highSpeed" size="7" min="0" max="150" value="100">
+			low: <input type="number" name="lowSpeed" size="7" min="0" max="60" value="0">
+			high: <input type="number" name="highSpeed" size="7" min="0" max="60" value="60">
 			<br>
 		</fieldset>
 		<fieldset id="date" style="display:none">
@@ -73,25 +73,25 @@
 </div>
 
 <script>
-	document.getElementById('aliasCheck').onchange = function() {
+	document.getElementById('aliasCheckid').onchange = function() {
    		document.getElementById('alias').style.display = this.checked ? 'block' : 'none';
 	};
-	document.getElementById('locCheck').onchange = function() {
+	document.getElementById('locCheckid').onchange = function() {
     	document.getElementById('loc').style.display = this.checked ? 'block' : 'none';
 	};
-	document.getElementById('tempCheck').onchange = function() {
+	document.getElementById('tempCheckid').onchange = function() {
     	document.getElementById('temp').style.display = this.checked ? 'block' : 'none';
 	};
-	document.getElementById('humidCheck').onchange = function() {
+	document.getElementById('humidCheckid').onchange = function() {
     	document.getElementById('humid').style.display = this.checked ? 'block' : 'none';
 	};
-	document.getElementById('lightCheck').onchange = function() {
+	document.getElementById('lightCheckid').onchange = function() {
     	document.getElementById('light').style.display = this.checked ? 'block' : 'none';
 	};
-	document.getElementById('windCheck').onchange = function() {
+	document.getElementById('windCheckid').onchange = function() {
     	document.getElementById('wind').style.display = this.checked ? 'block' : 'none';
 	};
-	document.getElementById('dateCheck').onchange = function() {
+	document.getElementById('dateCheckid').onchange = function() {
     	document.getElementById('date').style.display = this.checked ? 'block' : 'none';
 	};
 
